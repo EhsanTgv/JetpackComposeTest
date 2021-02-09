2b92046824ece2c6379fa3a1fe3cfeeb0bb9c172
EhsanTgv <EhsanTgv@gmail.com>
2021-02-09 14:48:51 +0330

    Implement a Box

--- app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetest/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.jetpackcomposetest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             JetpackComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    RowExample()
+                    BoxExample()
                 }
             }
         }
@@ -44,6 +49,33 @@ fun RowExample() {
     Row {
         Text("Hello World!")
         Text("Hello World!2")
+    }
+}
+
+@Composable
+fun BoxExample() {
+    Box(Modifier.fillMaxSize()) {
+        Text("This is first text", modifier = Modifier.align(Alignment.TopCenter))
+        Box(
+            Modifier
+                .align(Alignment.BottomCenter)
+                .preferredHeight(
+                    50.dp
+                )
+                .preferredWidth(
+                    50.dp
+                )
+                .background(Color.Blue)
+        )
+        Text("This is second text", modifier = Modifier.align(Alignment.Center))
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(12.dp),
+            onClick = {}
+        ) {
+            Text("+")
+        }
     }
 }
 
